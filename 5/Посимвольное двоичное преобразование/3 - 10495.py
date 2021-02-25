a838294987bf46c150e1606d0fd51ed6034a81a1
YarikMix <43493788+YarikMix@users.noreply.github.com>
2021-02-26 00:02:30 +0300

    Добавил 5 задачи

---
+++ 5/Посимвольное двоичное преобразование/3 - 10495.py
@@ -0,0 +1,15 @@
+def g(s):
+	"""Возвращает остаток от деления суммы цифр на 2"""
+	return str(sum([int(i) for i in s]) % 2)
+
+def f(N):
+	"""Алгоритм из условия задачи"""
+	R = format(N, "b")
+	R += g(R)
+	R += g(R)
+	return int(R, 2)
+
+for N in range(1, 100):
+	print(f"N = {N}, R = {f(N)}")
+
+# Ответ: 25
