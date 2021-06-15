97483fd9cbffae819ab2209c870c859d4d6f79fe
YarikMix <43493788+YarikMix@users.noreply.github.com>
2021-06-15 23:11:58 +0300

    Обновил 26 задачи

--- 26/Партии грузов (33496) (Недоделал)/main.py
+++ 26/Партии грузов (33496) (Недоделал)/main.py
@@ -1,18 +1,19 @@
 # Решение №1
 with open("input.txt") as f:
 	N, M = map(int, f.readline().split())
-	arr, res = [], []
+	res = []  # 210 <= Грузы <= 220
+	arr = []  # Остальные грузы
 
-	# Все грузы весом от 210 до 220 кг помещаем в список res
-	# Остальные грузы помещаем в список arr
 	for _ in range(N):
-		cargo = int(f.readline())
-		res.append(cargo) if 210 <= cargo <= 220 else arr.append(cargo)
+		weight = int(f.readline())
+		if 210 <= weight <= 220:
+			res.append(weight)
+		else:
+			arr.append(weight)
 
-	arr.sort()
-	res.sort()		
+	arr.sort()  # Сортируем по возрастанию
 
-	for i in range(len(arr)-1):
+	for i in range(len(arr) - 1):
 		if sum(res) + arr[i] <= M:
 			res.append(arr[i])
 		elif sum(res) - arr[i-1] + arr[i] <= M:
@@ -23,6 +24,7 @@ with open("input.txt") as f:
 			res.append(arr[i+1])
 		else:
 			break
+
 	print(len(res), sum(res) + 1)
 
 
--- 26/Спутник Фотон/main.py
+++ 26/Спутник Фотон/main.py
@@ -6,9 +6,10 @@ with open("input.txt") as f:
 	N, K = map(int, f.readline().split())
 	arr = [int(f.readline()) for _ in range(N)]
 
-arr.sort()
-arr = arr[K:N-K]
-avg = sum(arr) // len(arr)
-print(max(arr), avg)
+	arr.sort()
+	arr = arr[K:N-K]
+	avg = sum(arr) // len(arr)
+	print(max(arr), avg)
+
 
 # Ответ: 957 501
