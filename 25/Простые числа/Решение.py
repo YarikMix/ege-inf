c3fe28c4187eee544e1f1c2a24fe6c437a80cdd5
YarikMix <43493788+YarikMix@users.noreply.github.com>
2021-06-23 21:35:34 +0300

    Обновил 25 задачи

--- 25/Простые числа/Решение.py
+++ 25/Простые числа/Решение.py
@@ -1,17 +1,19 @@
 def is_prime(n):
     """Проверка числа на простоту"""
     d = 2
-    while d*d <= n:
-        if n % d == 0: return False
+    while d * d <= n:
+        if n % d == 0: 
+        	return False
         d += 1
     return True
 
-r = range(245690, 245756+1)
-for i, n in enumerate(r, start=1):
+for i, n in enumerate(range(245690, 245756+1), start=1):
     if is_prime(n):
-        print(f"{i} {n}")
+        print(i, n)
+
 
 """
+Ответ:
 22 245711
 30 245719
 34 245723
